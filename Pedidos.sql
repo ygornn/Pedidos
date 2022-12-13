da41487a253afc01321f812265f2e7f4d29eaaf3
Ygor Nathan Nolli <ygor.nolli.ifc.riodosul@gmail.com>
2022-12-12 21:18:55 -0300

    Versão Final

--- Pedidos.sql
+++ Pedidos.sql
@@ -92,9 +92,8 @@ codigo_pedido INT NOT NULL,
 codigo_lanche INT,
 quantidade INT,
 PRIMARY KEY(codigo_pedido, codigo_lanche),
-FOREIGN KEY (codigo_pedido) REFERENCES pedido(codigo) ON UPDATE CASCADE ON DELETE CASCADE,
-FOREIGN KEY (codigo_lanche) REFERENCES lanche(codigo) ON UPDATE CASCADE ON DELETE CASCADE
-);
+FOREIGN KEY (codigo_pedido) REFERENCES pedido(codigo) ON UPDATE CASCADE,
+FOREIGN KEY (codigo_lanche) REFERENCES lanche(codigo) ON UPDATE CASCADE);
 
 CREATE TABLE pedido_bebida (
 codigo_pedido INT NOT NULL,
@@ -104,20 +103,3 @@ PRIMARY KEY(codigo_pedido, codigo_bebida),
 FOREIGN KEY (codigo_pedido) REFERENCES pedido(codigo) ON UPDATE CASCADE ,
 FOREIGN KEY (codigo_bebida) REFERENCES bebida(codigo) ON UPDATE CASCADE 
 );
-
-SELECT pedido.codigo, nome_cliente, pizza.sabor as pizza, lanche.sabor as lanche, bebida.sabor as bebida, pedido_pizza.quantidade as p_qnt,
-    pedido_lanche.quantidade as l_qnt, pedido_bebida.quantidade as b_qnt, hora,
-    pizza.valor*pedido_pizza.quantidade + lanche.valor*pedido_lanche.quantidade + bebida.valor*pedido_bebida.quantidade as total
-    FROM pedido JOIN cliente ON pedido.codigo_cliente = cliente.codigo JOIN pedido_pizza ON pedido.codigo=pedido_pizza.codigo_pedido
-    JOIN pizza ON pedido_pizza.codigo_pizza = pizza.codigo JOIN pedido_lanche ON pedido.codigo=pedido_lanche.codigo_pedido
-    JOIN lanche ON pedido_lanche.codigo_lanche = lanche.codigo JOIN pedido_bebida ON pedido.codigo=pedido_bebida.codigo_pedido
-    JOIN bebida ON pedido_bebida.codigo_bebida = bebida.codigo;
-    
-SELECT * FROM pedido_bebida;
-SELECT * FROM pedido_pizza; 
-SELECT * FROM pedido_lanche;     
-
-DROP TABLE pedido;
-DROP TABLE pedido_pizza;
-DROP TABLE pedido_lanche;
-DROP TABLE pedido_bebida;
